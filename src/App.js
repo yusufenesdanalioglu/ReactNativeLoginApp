6d9a34c2c200a5912b103f3391238cfa17033190
yedanalioglu <41891933+yedanalioglu@users.noreply.github.com>
2021-03-01 01:22:36 +0300

    giriş yaptıktan sonra tekrar logout diyerek login ekranına dönme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Button} from 'react-native';
 import firebase from 'firebase';
 import Banner from './components/banner'
 import LoginForm from './components/loginForm'
-
-
+import Spinner from './components/common/Spinner'
 
 class App extends Component{
+  state = {
+    loggedIn:null,
+  }
+
   componentDidMount(){
       firebase.initializeApp({
         apiKey: "AIzaSyC1cADEEn4dlOPAq7eeQVs_XO0_0rS9Z8w",
@@ -17,13 +20,39 @@ class App extends Component{
         appId: "1:641048891368:web:b29e9743bd3d48a9e89e5d",
         measurementId: "G-0E2VK3CXZ0"
       });
+
+      firebase.auth().onAuthStateChanged((user) => {
+        const loggedIn = user ? true : false;
+        this.state({
+          loggedIn,
+        })
+      })
+  }
+
+  renderContent(){
+    const {loggedIn} = this.state;
+    switch (loggedIn) {
+      case true:
+        return (
+          <Button
+           onPress={() => firebase.auth().signOut()}
+           title="Logout"
+           color="#E3797C" />
+        )
+      case false:
+        return (
+          <LoginForm />
+        )
+      default:
+        <Spinner />
+    }
   }
 
   render(){
-    return(
+    return (
       <View style={styles.appContainer}>
         <Banner text="Sign In"  />
-        <LoginForm />
+          {this.renderContent()}
       </View>
     )
   }
